5a5a1b1feb35f8c8ad1c01381a78565435ad2302
ignius299792458 <bogati.mahesh.299.792.458@gmail.com>
2025-04-12 18:23:47 +0545

    📦 DOCKER: containerization - Dockerfile, docker-comose

---
+++ src/main/java/com/ignius/spring_security/controller/ServerHealthController.java
@@ -0,0 +1,17 @@
+package com.ignius.spring_security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/health")
+public class ServerHealthController {
+
+    @GetMapping
+    public Mono<ResponseEntity<String>> health() {
+        return Mono.just(ResponseEntity.ok("OK"));
+    }
+}
